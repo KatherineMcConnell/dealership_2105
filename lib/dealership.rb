698a57479e8427dab8a3f399f21f6dfac97719b2
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-05-20 19:47:42 -0600

    Thursday Morning Practice with Madeleine & Independent Evening Practice

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,1 +1,54 @@
+class Dealership
+  attr_reader :name,
+              :address,
+              :inventory
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
 
+  def inventory_count
+    @inventory.size
+  end
+
+  def add_car(car)
+    @inventory << car
+  end
+
+  def has_inventory?
+    !@inventory.empty?
+  end
+
+  def cars_by_make(make)
+    @inventory.find_all { |car| car.make == make }
+  end
+
+  def total_value
+    @inventory.sum {|car| car.total_cost}
+  end
+
+  def details
+    details_h = {}
+    details_h["total_value"]= total_value
+    details_h["address"]= @address
+    details_h
+  end
+
+  def average_price_of_car
+    (total_value/inventory_count).to_s.insert(-4,',')
+  end
+
+  def cars_sorted_by_price
+    @inventory.sort_by {|car| car.total_cost}
+  end
+
+  def inventory_hash
+    cars_by_make = Hash.new{|hash,key| hash[key]=[]}
+    @inventory.each do |car|
+      cars_by_make[car.make] << car
+    end
+    cars_by_make
+  end
+
+end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,1 +1,35 @@
+require './lib/car'
 
+RSpec.describe Car do
+#Iteration 1
+  before(:each) do
+    @car = Car.new("Ford Mustang", 1500, 36)
+  end
+
+  it 'exists' do
+
+    expect(@car).to be_an_instance_of(Car)
+  end
+
+  it 'has attributes' do
+
+    expect(@car.make).to eq("Ford")
+    expect(@car.model).to eq("Mustang")
+    expect(@car.monthly_payment).to eq(1500)
+    expect(@car.loan_length).to eq(36)
+  end
+
+  it 'has total_cost' do
+
+    expect(@car.total_cost).to eq(54000)
+  end
+
+  it 'starts unpainted and can be painted' do
+
+    expect(@car.color).to eq(nil)
+    @car.paint!(:blue)
+
+    expect(@car.color).to eq(:blue)
+  end
+
+end
